2f6cab3367a138635c93602af9087532ae9f6277
Willard21 <zyx870@yahoo.com>
2021-12-11 13:00:49 -0600

    Day 11

---
+++ Day-11-A.js
@@ -0,0 +1,25 @@
+let input = document.body.innerText.trim().split('\n').map(row => row.split("").map(n => +n))
+let count = 0
+function flash(x, y) {
+	count++
+	for (let b = y - 1; b <= y + 1; b++) {
+		for (let a = x - 1; a <= x + 1; a++) {
+			if (input[b]?.[a] !== undefined && ++input[b][a] === 10) {
+				flash(a, b)
+			}
+		}
+	}
+}
+for (let i = 0; i < 100; i++) {
+	for (let y = 0; y < input.length; y++) {
+		for (let x = 0; x < input[y].length; x++) {
+			if (++input[y][x] === 10) flash(x, y)
+		}
+	}
+	for (let y = 0; y < input.length; y++) {
+		for (let x = 0; x < input[y].length; x++) {
+			if (input[y][x] > 9) input[y][x] = 0
+		}
+	}
+}
+console.log(count)
