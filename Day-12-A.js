8cebac9a4ffdee360c33914ff38879dd04b29995
Willard21 <zyx870@yahoo.com>
2021-12-12 13:26:10 -0600

    Remove redundant code

--- Day-12-A.js
+++ Day-12-A.js
@@ -1,51 +1,27 @@
-let input = document.body.innerText.trim().split('\n').map(row => row.split("-"))
-let map = {}
-let rooms = new Set(input.flat())
-for (let loc of rooms) {
-	map[loc] = new Set()
+const input = document.body.innerText.trim().split('\n').map(row => row.split("-"))
+const nodes = {}
+for (let value of new Set(input.flat())) {
+	nodes[value] = {
+		used: false,
+		reusable: value.charCodeAt(0) < 'a'.charCodeAt(0),
+		next: []
+	}
 }
-for (let row of input) {
-	map[row[0]].add(row[1])
-	map[row[1]].add(row[0])
+for (let [a, b] of input) {
+	nodes[a].next.push(nodes[b])
+	nodes[b].next.push(nodes[a])
 }
 
 let count = 0
-function callback() {
-	count++
-}
-
-function search(array, callback) {
-	class Node {
-		constructor(value, index) {
-			this.value = value
-			this.used = false
-			this.index = index
-			this.reusable = value.charCodeAt(0) < 'a'.charCodeAt(0)
-		}
-		addNodes(nodes) {
-			this.next = nodes
+function search(node) {
+	node.used = true
+	for (let next of node.next) {
+		if (next.reusable || !next.used) {
+			if (next === nodes.end) count++
+			else search(next)
 		}
 	}
-	const nodes = array.map((value, index) => new Node(value, index))
-	nodes.forEach(node => node.addNodes(nodes.filter(n => map[node.value].has(n.value))))
-
-	let stack = []
-	function search(node) {
-		node.used = true
-		stack.push(node.index)
-
-		if (node.value === "end") {
-			callback(stack)
-		} else {
-			for (let n of node.next) {
-				if (n.reusable || !n.used) search(n)
-			}
-		}
-
-		stack.pop()
-		node.used = false
-	}
-	search(nodes.filter(node => node.value === "start")[0])
+	node.used = false
+	return count
 }
-search(Array.from(rooms), callback)
-count
+search(nodes.start)
--- Day-12-B.js
+++ Day-12-B.js
@@ -1,57 +1,27 @@
-let input = document.body.innerText.trim().split('\n').map(row => row.split("-"))
-let map = {}
-let rooms = new Set(input.flat())
-for (let loc of rooms) {
-	map[loc] = new Set()
+const input = document.body.innerText.trim().split('\n').map(row => row.split("-"))
+const nodes = {}
+for (let value of new Set(input.flat())) {
+	nodes[value] = {
+		used: 0,
+		reusable: value.charCodeAt(0) < 'a'.charCodeAt(0),
+		next: []
+	}
 }
-for (let row of input) {
-	map[row[0]].add(row[1])
-	map[row[1]].add(row[0])
+for (let [a, b] of input) {
+	nodes[a].next.push(nodes[b])
+	nodes[b].next.push(nodes[a])
 }
 
 let count = 0
-function callback() {
-	count++
-}
-
-// I wrote this for a 2015 AoC challenge, and just modified it a tad with the search confitions. It was originally for searching permutations.
-function search(array, callback) {
-	class Node {
-		constructor(value, index) {
-			this.value = value
-			this.used = 0
-			this.index = index
-			this.reusable = value.charCodeAt(0) < 'a'.charCodeAt(0)
-		}
-		addNodes(nodes) {
-			this.next = nodes
+function search(node, reused) {
+	node.used++
+	for (let next of node.next) {
+		if (next.reusable || !next.used || !reused && next !== nodes.start) {
+			if (next === nodes.end) count++
+			else search(next, reused || next.used && !next.reusable)
 		}
 	}
-	const nodes = array.map((value, index) => new Node(value, index))
-	nodes.forEach(node => node.addNodes(nodes.filter(n => map[node.value].has(n.value))))
-
-	let stack = []
-	let reused = false
-	function search(node) {
-		node.used++
-		stack.push(node.index)
-		if (!node.reusable && node.used > 1) reused = true
-
-		if (node.value === "end") {
-			callback(stack)
-		} else {
-			for (let n of node.next) {
-				if (n.reusable || !n.used || !reused && n.value !== "start") search(n)
-			}
-		}
-
-		stack.pop()
-		node.used--
-		if (!node.reusable && node.used === 1) reused = false
-	}
-	search(nodes.filter(node => node.value === "start")[0])
+	node.used--
+	return count
 }
-
-search(Array.from(rooms), callback)
-
-count
+search(nodes.start)
