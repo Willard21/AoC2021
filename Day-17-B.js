1b56b08a817e80926f9ea60168207d5e85685f50
Willard21 <zyx870@yahoo.com>
2021-12-17 21:30:54 +0000

    Replace magic numbers

--- Day-17-B.js
+++ Day-17-B.js
@@ -1,18 +1,18 @@
 let [minX, maxX, minY, maxY] = document.body.innerText.match(/x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/).slice(1).map(n => +n)
 let count = 0
-for (let vxs = 23; vxs <= 292; vxs++) {
-	for (let vys = -68; vys <= 67; vys++) {
+for (let vxs = Math.floor(Math.sqrt(2 * minX)); vxs <= maxX; vxs++) {
+	for (let vys = minY; vys <= -minY - 1; vys++) {
 		let vy = vys
 		let vx = vxs
 		let x = 0
 		let y = 0
-		while(x <= 292 && y > -68) {
+		while(x <= maxX && y > minY) {
 			x += vx
 			y += vy
-			vy -= 1
+			vy--
 			if (vx > 0) vx--
 			else if (vx < 0) vx++
-			if (x >= 269 && x <= 292 && y <= -44 && y >= -68) {
+			if (x >= minX && x <= maxX && y <= maxY && y >= minY) {
 				count++
 				break
 			}
